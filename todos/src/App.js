98e6fdebe97b759f699e42aaf38c0058b987236b
Ferdaus Zaman Polok <ferdaus.study@gmail.com>
2023-01-18 15:27:22 +0600

    Load More button component done with css

--- todos/src/App.js
+++ todos/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import TopButtons from "./components/topButtons";
 import AllTodos from "./components/allTodos";
+import LoadmoreBtn from "./components/loadmoreBtn";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <TopButtons />
       <AllTodos />
+      <LoadmoreBtn />
     </div>
   );
 }
---
+++ todos/src/components/loadmoreBtn.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LoadmoreBtn() {
+  return (
+    <div className="load-more">
+        <button className="btn-load-more">
+            Load More
+        </button>
+    </div>
+  )
+}
+
+export default LoadmoreBtn
