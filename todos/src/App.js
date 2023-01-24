af370d741b2de711d5f325fcbdbf9eb97d1b541a
Ferdaus Zaman Polok <ferdaus.zaman@welldev.io>
2023-01-24 16:33:32 +0600

    Relative path import in App.js

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,9 +1,8 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import TopContent from "./components/TopContent";
-import Todos from "./components/Todos";
-import LoadMoreBtn from "./components/LoadMoreBtn";
-import EmptyTaskList from "./components/EmptyTaskList";
+import "App.css";
+import Navbar from "components/Navbar";
+import TopContent from "components/TopContent";
+import Todos from "components/Todos";
+import LoadMoreBtn from "components/LoadMoreBtn";
 
 function App() {
   return (
