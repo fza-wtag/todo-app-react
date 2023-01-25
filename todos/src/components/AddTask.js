d1f6e68614675cfc51300f16468ab3172bdcff54
fza-wtag <ferdaus.zaman@welldev.io>
2023-01-25 13:37:24 +0600

    Adding todo into redux store

--- todos/src/components/AddTask.js
+++ todos/src/components/AddTask.js
@@ -1,11 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import del from "icons/delete.svg";
 import "styles/addTask.css";
 import { TopContentContext } from "components/TopContent";
+import { addTodo } from "actions/index";
+import { useDispatch } from "react-redux";
 
 const AddTask = () => {
   const { isAddTaskVisible, setIsAddTaskVisible } =
     useContext(TopContentContext);
+  const [inputData, setInputData] = useState("");
+  const dispatch = useDispatch();
 
   return (
     <div className="todo">
@@ -13,10 +17,17 @@ const AddTask = () => {
         <textarea
           className="textarea__edit-text"
           placeholder="Add new task..."
+          value={inputData}
+          onChange={(event) => setInputData(event.target.value)}
         ></textarea>
       </div>
       <div className="todo__add_del">
-        <button className="btn btn__save_button">Add Task</button>
+        <button
+          className="btn btn__save_button"
+          onClick={() => dispatch(addTodo(inputData), setInputData(""))}
+        >
+          Add Task
+        </button>
         <button
           className="todo__icon-btn"
           onClick={() => setIsAddTaskVisible(!isAddTaskVisible)}
