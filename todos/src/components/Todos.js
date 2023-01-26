6200dbaa9e94b8a72ceb8a1ef09619553e85ad5a
fza-wtag <ferdaus.zaman@welldev.io>
2023-01-26 09:52:08 +0600

    Show/ hide Empty tasklist UI

--- todos/src/components/Todos.js
+++ todos/src/components/Todos.js
@@ -4,27 +4,31 @@ import Task from "components/Task";
 import "styles/todos.css";
 import { TopContentContext } from "components/TopContent";
 import { useSelector } from "react-redux";
+import EmptyTaskList from "./EmptyTaskList";
 
 function Todos() {
   const { isAddTaskVisible, setIsAddTaskVisible } =
     useContext(TopContentContext);
   const list = useSelector((state) => state.todoReducers.list);
   return (
-    <div className="all-todos">
-      {isAddTaskVisible ? <AddTask /> : null}
-      {list
-        .slice()
-        .reverse()
-        .map((elem) => {
-          return elem.data ? (
-            <Task
-              myKey={elem.id}
-              name={elem.data}
-              date={elem.date}
-              isCompleted={elem.isCompleted}
-            />
-          ) : null;
-        })}
+    <div>
+      <div className="all-todos">
+        {isAddTaskVisible ? <AddTask /> : null}
+        {list
+          .slice()
+          .reverse()
+          .map((elem) => {
+            return elem.data ? (
+              <Task
+                myKey={elem.id}
+                name={elem.data}
+                date={elem.date}
+                isCompleted={elem.isCompleted}
+              />
+            ) : null;
+          })}
+      </div>
+      {list.length === 0 && !isAddTaskVisible ? <EmptyTaskList /> : null}
     </div>
   );
 }
