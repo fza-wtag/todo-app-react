c1beae551135e40d5df9651083e19b7469c58b34
fza-wtag <ferdaus.zaman@welldev.io>
2023-02-09 14:55:45 +0600

    Fix as per last review

--- todos/src/components/LoadMoreBtn.js
+++ todos/src/components/LoadMoreBtn.js
@@ -4,7 +4,6 @@ import { useSelector, useDispatch } from "react-redux";
 import { updateCurrentPage } from "actions";
 import { LOAD_MORE, SHOW_LESS } from "constants";
 
-
 function LoadMoreBtn(props) {
   const dispatch = useDispatch();
   const currentPage = useSelector(
@@ -19,14 +18,17 @@ function LoadMoreBtn(props) {
     dispatch(updateCurrentPage(1));
   };
 
+  const typeLoadMore = props.type === LOAD_MORE;
+  const typeShowLess = props.type === SHOW_LESS;
+
   return (
     <div className="load-more">
-      {props.type === LOAD_MORE && (
+      {typeLoadMore && (
         <button className="load-more__button" onClick={handleLoadMore}>
           {props.type}
         </button>
       )}
-      {props.type === SHOW_LESS && (
+      {typeShowLess && (
         <button className="load-more__button" onClick={handleShowLess}>
           {props.type}
         </button>
