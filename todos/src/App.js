45ca8d79bdbc7ffedea60e5f7367bf80f0f93c77
fza-wtag <ferdaus.zaman@welldev.io>
2023-02-15 16:24:24 +0600

    Add splash screen with a loading bar above for first time loading

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,33 +1,44 @@
-import { React, useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import "App.css";
 import Navbar from "components/Navbar";
 import Todos from "components/Todos";
-import { Provider } from "react-redux";
-import store from "store";
 import TopContent from "components/TopContent";
 import { ToastContainer } from "react-toastify";
 import { useDispatch } from "react-redux";
 import { getCurrentTodos } from "supabaseData";
 import { addInitialData } from "actions/index";
+import { setLoadingState } from "actions";
+import { useSelector } from "react-redux";
+import SplashScreen from "components/SplashScreen";
 
 function App() {
+  const loadingState = useSelector(
+    (state) => state.laodingReducer.loadingState
+  );
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchData = async () => {
+      dispatch(setLoadingState(true));
       const initialData = await getCurrentTodos();
+      dispatch(setLoadingState(false));
       dispatch(addInitialData(initialData));
     };
     fetchData();
   }, [dispatch]);
 
-  return (
-    <div>
-      <Navbar />
-      <ToastContainer />
-      <TopContent />
-      <Todos />
-    </div>
+  return loadingState ? (
+    <SplashScreen />
+  ) : (
+    <>
+      <Fragment>
+        <Navbar />
+        <ToastContainer />
+        <TopContent />
+        <Todos />
+      </Fragment>
+    </>
   );
 }
 
---
+++ todos/src/components/SplashScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import splashLogo from "icons/sideLogo.svg";
+import "styles/splashScreen.css";
+
+function SplashScreen() {
+  return (
+    <div className="splash-screen">
+      <div className="loading-bar"></div>
+      <img src={splashLogo} alt="icon"></img>
+    </div>
+  );
+}
+
+export default SplashScreen;
