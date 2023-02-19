4f00b25a7fb03371c98a4327164992b726844360
fza-wtag <ferdaus.zaman@welldev.io>
2023-02-19 10:02:27 +0600

    Key url format

--- todos/src/supabaseData.js
+++ todos/src/supabaseData.js
@@ -1,9 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  "https://aemdlybecnilhqobaiwn.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlbWRseWJlY25pbGhxb2JhaXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYyNjE0NDQsImV4cCI6MTk5MTgzNzQ0NH0.MawqvRVONrJWmOTjAmkSoBKLMPAl0dyiw2k2nUEt3IE"
-);
+const projectURL = "https://aemdlybecnilhqobaiwn.supabase.co";
+const anonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlbWRseWJlY25pbGhxb2JhaXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYyNjE0NDQsImV4cCI6MTk5MTgzNzQ0NH0.MawqvRVONrJWmOTjAmkSoBKLMPAl0dyiw2k2nUEt3IE";
+
+const supabase = createClient(projectURL, anonKey);
 
 export async function getCurrentTodos() {
   const tableData = await supabase.from("todo_data2").select();
