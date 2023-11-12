663fb643051652929ab1c8ad87fe1de698181e13
fza-wtag <ferdaus.zaman@welldev.io>
2023-11-12 18:46:04 +0600

    Move API Key to env

--- todos/src/supabase.js
+++ todos/src/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const projectURL = "https://aemdlybecnilhqobaiwn.supabase.co";
 const anonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlbWRseWJlY25pbGhxb2JhaXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYyNjE0NDQsImV4cCI6MTk5MTgzNzQ0NH0.MawqvRVONrJWmOTjAmkSoBKLMPAl0dyiw2k2nUEt3IE";
+  process.env.REACT_APP_SUPABASE_API_KEY;
 let supabaseInstance = null;
 
 class SupabaseSingleton {
